f820e7266875ca9e2b9716323ba2a9ddf83633e0
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-04-04 12:36:53 +0200

    4

--- day2/prove_mattina/assets/script/script.js
+++ day2/prove_mattina/assets/script/script.js
@@ -1,14 +1,15 @@
-const btn = document.getElementById('verfica');
+const btn = document.getElementById('verifica');
 var eta;
 var stato;
 btn.addEventListener('click', function () {
     let nome = document.getElementById('nome');
     let anno = document.getElementById('anno');
+
     calcolaEta(anno.value);
     verifica();
-    scrivi(nome.value)
+    scrivi(nome.value);
     cancellaform(nome, anno);
-});
+})
 function calcolaEta(anno) {
     eta= 2023 - anno;
 }
@@ -16,8 +17,8 @@ function verifica() {
     stato = (eta >= 18)? 'maggiorenne': 'minorenne';
 }
 function scrivi(nome) {
-    document.getElementById('mionome').innerHTML = 'Ciao' + nome;
-    document.getElementById('miaverifica').innerHTML = 'La tua età è' + eta + 'anni, sei' + stato;
+    document.getElementById('mioNome').innerHTML = 'Ciao ' + nome;
+    document.getElementById('miaVerifica').innerHTML = 'La tua età è ' + eta + ' anni, sei ' + stato;
 }
 function cancellaform(nome, anno) {
     nome.value = '';
